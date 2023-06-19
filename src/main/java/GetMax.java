0cd0b5ea3dd51645237775e906b1ad4d6180a490
Artem Synytsia <synytsia.artem@yahoo.com>
2023-06-19 00:14:55 +0000

    WkThree_DayFive_CodingChallenge

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,20 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        
+        // Check if the array is valid
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        
+        int largestValue = arr[0];
+        
+        // The loop to search through the array
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largestValue) {
+                largestValue = arr[i];
+            }
+        }
+        return largestValue;
     }
 }
